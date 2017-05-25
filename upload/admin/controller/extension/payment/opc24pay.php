f21606b04a161f517e4fce48701a08e55faf0437
24pay <durcak@24-pay.eu>
2017-05-25 08:34:57 +0200

    Fix

--- upload/admin/controller/extension/payment/opc24pay.php
+++ upload/admin/controller/extension/payment/opc24pay.php
@@ -1,262 +1,158 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+class ControllerExtensionPaymentOpc24pay extends Controller
+{
+    private $error = array();
+    private $settings = array();
 
-/**
- * Description of paygate_24pay
- *
- * @author durcak
- */
+    //Config page
+    public function index()
+    {
+        $this->load->language('extension/payment/opc24pay');
+        $this->document->setTitle($this->language->get('heading_title'));
+        $this->load->model('setting/setting');
 
-class ControllerExtensionPaymentOpc24Pay extends Controller{
-
-		const PAY_BUTTON = 'https://www.24-pay.sk/wp-content/themes/24pay/images/logo.gif';
-
-        const VERSION = '2.3.0';
-
-
-        public function index()
-        {
-            $data['opc24pay_button'] = self::PAY_BUTTON;
-            $data['action'] = $this->url->link('extension/payment/opc24pay/pay','', true);
-
-            return $this->load->view('extension/payment/opc24pay', $data);
+        //new config
+        if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validate()) {
+            $this->model_setting_setting->editSetting('opc24pay', $this->request->post);
+            $this->session->data['success'] = $this->language->get('text_success');
+            $this->response->redirect($this->url->link('extension/extension', 'token=' . $this->session->data['token'], true));
         }
         
-        public function pay(){
-            $return = array();
-            
-            if ($this->session->data['payment_method']['code'] == 'opc24pay') {
-                $this->load->model('checkout/order');
-                $this->load->model('setting/setting');
-                
-                $setting = $this->model_setting_setting->getSetting('opc24pay');
-                $data = array();
-                $data['Test'] = $setting['opc24pay_test'];
-                $data['Mid'] = $setting['opc24pay_mid'];
-                $data['EshopId'] = $setting['opc24pay_eshopid'];
-                $data['Key'] = $setting['opc24pay_key'];
-                
-                $order_info = $this->model_checkout_order->getOrder($this->session->data['order_id']);
-                
-                $data['FirstName'] = $order_info['payment_firstname'];
-                $data['FamilyName'] = $order_info['payment_lastname'];
-                $data['MsTxnId'] = $order_info['order_id'];
-                $data['Amount'] = number_format((float)$order_info['total'], 2, '.', '');
-                $data['CurrAlphaCode'] = $order_info['currency_code'];
-                
-                if ($order_info['customer_id']==0)
-                    $data['ClientId'] = "NOREG".$order_info['customer_id'];
-                else
-                    $data['ClientId'] = "OPC".$order_info['customer_id'];
-                $data['Email'] = $order_info['email'];
-                $data['Country'] = $order_info['payment_iso_code_3'];
-                
-                $data['Timestamp'] = date("Y-m-d H:i:s");
-
-                $data['NURL'] = $this->url->link('extension/payment/opc24pay/nurl','', true);
-                $data['RURL'] = $this->url->link('extension/payment/opc24pay/rurl','', true);
-                
-                $return['form'] = $this->buildForm($data);
-                $return['status'] = "SUCCESS";
-            }
-            else{
-                
-                $return['message'] = "Niečo sa pokazilo";
-                $return['code'] = $this->session->data['payment_method']['code'];
-                $return['status'] = "fail";
-            }
-            
-            
-            echo json_encode($return);
-            exit();
-        }
+        $this->setting = $this->model_setting_setting->getSetting('opc24pay');
         
-        private function buildForm($data){
-            $sign = $this->sign($data);
-            
-            $formStr = "";
-            if ($data['Test'] == 1)
-                $formStr = "<form id='opc24pay-submitform' method='post' action='https://doxxsl-staging.24-pay.eu/pay_gate/paygt' >";
-            else
-                $formStr = "<form id='opc24pay-submitform' method='post' action='https://admin.24-pay.eu/pay_gate/paygt' >";
-            
-            $formStr .= "<input type='hidden' name='Mid' value='".$data['Mid']."' />";
-            $formStr .= "<input type='hidden' name='EshopId' value='".$data['EshopId']."' />";
-            $formStr .= "<input type='hidden' name='MsTxnId' value='".$data['MsTxnId']."' />";
-            $formStr .= "<input type='hidden' name='Amount' value='".$data['Amount']."' />";
-            $formStr .= "<input type='hidden' name='CurrAlphaCode' value='".$data['CurrAlphaCode']."' />";
-            $formStr .= "<input type='hidden' name='ClientId' value='".$data['ClientId']."' />";
-            $formStr .= "<input type='hidden' name='FirstName' value='".$data['FirstName']."' />";
-            $formStr .= "<input type='hidden' name='FamilyName' value='".$data['FamilyName']."' />";
-            $formStr .= "<input type='hidden' name='Email' value='".$data['Email']."' />";
-            $formStr .= "<input type='hidden' name='Country' value='".$data['Country']."' />";
-            $formStr .= "<input type='hidden' name='Timestamp' value='".$data['Timestamp']."' />";
-            $formStr .= "<input type='hidden' name='NURL' value='".$data['NURL']."' />";
-            $formStr .= "<input type='hidden' name='RURL' value='".$data['RURL']."' />";
-            $formStr .= "<input type='hidden' name='Sign' value='".$sign."' />";
-            $formStr .= "<input type='hidden' name='Debug' value='true' />";
-            $formStr .= "</form>";
-            
-            return $formStr;
-        }
+        $data['error_warning'] = false;
+        //language data
+        $data['heading_title'] = $this->language->get('heading_title');
         
-        private function sign($data){
-            $message = $data['Mid'].$data['Amount'].$data['CurrAlphaCode'].$data['MsTxnId'].$data['FirstName'].$data['FamilyName'].$data['Timestamp'];
-
-            $hash = hash("sha1", $message, true);
-            $iv = $data['Mid'] . strrev($data['Mid']);
-
-            $key = pack('H*', $data['Key']);
-
-            if ( PHP_VERSION_ID >= 50303 && extension_loaded( 'openssl' ) ) {
-                    $crypted = openssl_encrypt( $hash, 'AES-256-CBC', $key, 1, $iv );
-            } else {
-                    $crypted = mcrypt_encrypt( MCRYPT_RIJNDAEL_128, $key, $hash, MCRYPT_MODE_CBC, $iv );
-            }
-            $sign = strtoupper(bin2hex(substr($crypted, 0, 16)));
-            
-            return $sign;
-        }
+        $data['entry_status'] = $this->language->get('entry_status');
+        $data['entry_order'] = $this->language->get('entry_order');
+        $data['entry_test'] = $this->language->get('entry_test');
+        $data['entry_mid'] = $this->language->get('entry_mid');
+        $data['entry_eshopid'] = $this->language->get('entry_eshopid');
+        $data['entry_key'] = $this->language->get('entry_key');
         
-        public function rurl(){
-            $order_id = $this->request->get["MsTxnId"];
-            $total = $this->request->get["Amount"];
-            $currency_code = $this->request->get["CurrCode"];
-            $result = $this->request->get["Result"];
-
-            switch ($result) {
-                case "OK":
-                case "PENDING":
-                        $this->response->redirect($this->url->link('checkout/success'));
-                        break;
-
-                case "FAIL":
-                        $this->response->redirect($this->url->link("checkout/cart"));
-                        break;
-            }
-            die("Invalid arguments");
-        }
+		if (isset($this->error['error_mid'])) {
+			$data['error_mid'] = $this->error['error_mid'];
+		} else {
+			$data['error_mid'] = '';
+		}
         
+		
+		if (isset($this->error['error_eshopid'])) {
+			$data['error_eshopid'] = $this->error['error_eshopid'];
+		} else {
+			$data['error_eshopid'] = '';
+		}
         
-        public function nurl(){
-            if (isset($_POST["params"])) {
-                $params = $_POST["params"];	
-            } else {
-                echo "Invalid notification params";
-                die();
-            }
-
-            $twentyfourpay_notification = $this->parseNotification($params);
-
-            if (!$twentyfourpay_notification['Valid']){
-                die("Invalid response from 24pay gateway");
-                print_r($twentyfourpay_notification);
-            }
-
-            $this->load->model('checkout/order');
-
-            $order_id = $twentyfourpay_notification['MsTxnId'];
-            
-            $order = $this->model_checkout_order->getOrder($order_id);
-            $result = $twentyfourpay_notification['Result'];
-
-            switch ($result) {
-                    case "PENDING":
-                            if (!$order["order_status_id"])
-                                    // change order status to pending (waiting for payment transation to be completed)
-                                    $this->model_checkout_order->addOrderHistory($order_id, 1, "24-pay: Notification with result PENDING");
-                            break;
-
-                    case "OK":
-                            if (!$order["order_status_id"] || $order["order_status_id"] == 1)
-                                    // change order status to precessing (already payed, waiting for shippment to be expedited)
-                                    $this->model_checkout_order->addOrderHistory($order_id, 2, "24-pay: Notification with result OK");
-                                    $this->model_checkout_order->addOrderHistory($order_id, 2, "24-pay: PAYMENT SUCCESSFULLY RECIEVED");
-                            break;
-
-                    case "FAIL": default:
-                            if (!$order["order_status_id"] || $order["order_status_id"] == 1)
-                                    // change to pending sttau with note about failed transaction
-                                    $this->model_checkout_order->addOrderHistory($order_id, 1, "24-pay: Notification with result FAIL");
-                            break;
-            }
-
-            die("OK");
-        }
+		
+		if (isset($this->error['error_key'])) {
+			$data['error_key'] = $this->error['error_key'];
+		} else {
+			$data['error_key'] = '';
+		}
         
-        private function computeSign($message, $mid, $key){
-            $hash = hash("sha1", $message, true);
-            $iv = $mid . strrev($mid);
-
-            $key = pack('H*', $key);
-
-            if ( PHP_VERSION_ID >= 50303 && extension_loaded( 'openssl' ) ) {
-                    $crypted = openssl_encrypt( $hash, 'AES-256-CBC', $key, 1, $iv );
-            } else {
-                    $crypted = mcrypt_encrypt( MCRYPT_RIJNDAEL_128, $key, $hash, MCRYPT_MODE_CBC, $iv );
-            }
-            $sign = strtoupper(bin2hex(substr($crypted, 0, 16)));
-            
-            return $sign;
-        }
+		
+		if (isset($this->error['error_sort_order'])) {
+			$data['error_sort_order'] = $this->error['error_sort_order'];
+		} else {
+			$data['error_sort_order'] = '';
+		}
+		
+		
         
-        private function parseNotification($params){
-            if (get_magic_quotes_gpc())
-                $params = stripslashes($params);
+        $data['button_save'] = $this->language->get('button_save');
+        $data['button_cancel'] = $this->language->get('button_cancel');
+        
+        $data['opc24pay_status'] = $this->setting['opc24pay_status'];
+        $data['opc24pay_sort_order'] = $this->setting['opc24pay_sort_order'];
+        $data['opc24pay_test'] = $this->setting['opc24pay_test'];
+        $data['opc24pay_mid'] = $this->setting['opc24pay_mid'];
+        $data['opc24pay_eshopid'] = $this->setting['opc24pay_eshopid'];
+        $data['opc24pay_key'] = $this->setting['opc24pay_key'];
+        
+        $data['text_edit'] = $this->language->get('text_edit');
+        // MANDATORY DATA
+        
+        //Breadcroumbs
+        $data['breadcrumbs'] = array();
+        $data['breadcrumbs'][] = array(
+            'text' => $this->language->get('text_home'),
+            'href' => $this->url->link('common/home', 'token=' . $this->session->data['token'], true)
+        );
 
-            $params = trim(preg_replace("/^\s*<\?xml.*?\?>/i", "", $params));
+        $data['breadcrumbs'][] = array(
+            'text' => $this->language->get('text_extension'),
+            'href' => $this->url->link('extension/extension', 'token=' . $this->session->data['token'] . '&type=payment', true)
+        );
 
-            $xml = new SimpleXMLElement($params);
-            
-            $result = array();
-            
-            if ($xml->count()==1){
-                
-                $this->load->model('setting/setting');
-                
-                $setting = $this->model_setting_setting->getSetting('opc24pay');
-                
-                $mid = $setting['opc24pay_mid'];
-                $key = $setting['opc24pay_key'];
-                
-                // SIGN
-                $node = $xml[0];
-                $attributes = $node->attributes();
-                $result['Sign'] = (string) $attributes["sign"];
-                // AMOUNT
-                $result['Amount'] = (string) $xml->Transaction->Presentation->Amount;
-                // CURRENCY
-                $result['Currency'] = (string) $xml->Transaction->Presentation->Currency;
-                // PSPTXNID
-                $result['PspTxnId'] = $xml->Transaction->Identification->PspTxnId;
-                // MSTXNID
-                $result['MsTxnId'] = (string) $xml->Transaction->Identification->MsTxnId;
-                // TIMESTAMP
-                $result['Timestamp'] =  (string) $xml->Transaction->Processing->Timestamp;
-                // RESULT
-                $result['Result'] = (string) $xml->Transaction->Processing->Result;
-                
-                $message = $mid.$result['Amount'].$result['Currency'].$result['PspTxnId'].$result['MsTxnId'].$result['Timestamp'].$result['Result'];
-                
-                $signCandidate = $this->computeSign($message,$mid,$key);
-                if ($signCandidate == $result['Sign']){
-                    $result['Valid'] = true;
-                }
-                else{
-                    $result['ValidSign'] = $signCandidate;
-                }
-            }
-            else{
-                $result['Valid'] = false;
-            }
-            
-            return $result;
+        $data['breadcrumbs'][] = array(
+            'text' => $this->language->get('heading_title'),
+            'href' => $this->url->link('extension/payment/opc24pay', 'token=' . $this->session->data['token'], true)
+        );
+        
+        $data['action'] = $this->url->link('extension/payment/opc24pay', 'token=' . $this->session->data['token'], true);
+        $data['cancel'] = $this->url->link('extension/extension', 'token=' . $this->session->data['token'], true);
+        
+        $data['header'] = $this->load->controller('common/header');
+        $data['column_left'] = $this->load->controller('common/column_left');
+        $data['footer'] = $this->load->controller('common/footer');
+        
+        $this->response->setOutput($this->load->view('extension/payment/opc24pay', $data));
+
+    } //index
+
+
+    //validate
+    private function validate()
+    {
+        //permisions
+        if (!$this->user->hasPermission('modify', 'extension/payment/opc24pay')) {
+            $this->error['warning'] = $this->language->get('warning');
         }
-    
+        //check for errors
+		/*
+        if (!$this->request->post['opc24pay_test']) {
+            $this->error['error_test'] = $this->language->get('error_test');
+        }
+        if (!$this->request->post['opc24pay_status']) {
+            $this->error['error_status'] = $this->language->get('error_status');
+        }
+		*/
+        if (!$this->request->post['opc24pay_mid']) {
+            $this->error['error_mid'] = $this->language->get('error_mid');
+        }
+        if (!$this->request->post['opc24pay_eshopid']) {
+            $this->error['error_eshopid'] = $this->language->get('error_eshopid');
+        }
+        if (!$this->request->post['opc24pay_key']) {
+            $this->error['error_key'] = $this->language->get('error_key');
+        }
+        if (!$this->request->post['opc24pay_sort_order']) {
+            $this->error['error_sort_order'] = $this->language->get('error_sort_order');
+        }
+
+        return !$this->error;
+    }
+
+    public function install()
+    {
+        $this->load->model('setting/setting');
+        $this->settings = array(
+            'opc24pay_status' => 0,
+            'opc24pay_test' => 1,
+            'opc24pay_sort_order' => 1,
+            'opc24pay_mid' => "demoOMED",
+            'opc24pay_eshopid' => "11111111",
+            'opc24pay_key' => "1234567812345678123456781234567812345678123456781234567812345678",
+        );
+        $this->model_setting_setting->editSetting('opc24pay', $this->settings);
+        
+    }
+
+    public function uninstall()
+    {
+        $this->load->model('setting/setting');
+        $this->model_setting_setting->deleteSetting('opc24pay');
+    }
 }
-
---
+++ upload/catalog/language/sk-sk/extension/payment/opc24pay.php
@@ -0,0 +1,13 @@
+<?php
+
+$_['text_title'] = '24pay';
+$_['text_select_payment_method'] = 'Vyberte si platobnú metódu:';
+
+$_['text_waiting_for_payment_to_complete'] = 'Čakám na ukončenie platby';
+$_['text_payment_received'] = 'Platba bola prijatá';
+$_['text_transaction_failed'] = 'Platba bola neúspešná';
+$_['text_waiting_for_payment'] = 'Čakám na platby';
+
+
+// $_['text_reason'] 	= 'REASON';
+// $_['text_total']	= 'Shipping, Handling, Discounts & Taxes';
